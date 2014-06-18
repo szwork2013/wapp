b417a4b297957c69630095e14cc212de2757807c
spout <snoopyxdy@gmail.com>
2014-06-18 17:10:44 +0800

    remove dir

--- appiddcz3l1clc2/db_backup.js
+++ appiddcz3l1clc2/db_backup.js
@@ -1,38 +1,42 @@
 var schedule = require('node-schedule');
-var moment = require('moment')
+var moment = require('moment');
+var util = require('util');
 var fs = require('fs');
-var file = require('file');
+
 var path = require('path');
 var exec = require('child_process').exec
 
-var backup_path = 'd:\\wapp\\db_backup\\'
-var dbname = 'wapp'
+var backup_path = 'D:\\db_backup\\'
+var dbname = 'admin'
 
 if(!fs.existsSync(backup_path)){
-	file.mkdirsSync(backup_path);
+	fs.mkdirSync(backup_path);
 }
 
 
 var commond = function(){
-	var filename = moment().format('YYYY/MM/DD');
+	var filename = moment().format('YYYY-MM-DD');
 
-	child = exec(sprintf('mongodump -h 127.0.0.1  -d %s -o %s', dbname, backup_path+filename),
+	child = exec(util.format('mongodump -h 127.0.0.1  -d %s -o %s', dbname, backup_path+filename),
 	  function (error, stdout, stderr) {
 	    console.log('stdout: ' + stdout);
 	    console.log('stderr: ' + stderr);
 	    if (error !== null) {
 	      console.log('exec error: ' + error);
 	    }
+	    fs.chmodSync(backup_path+filename, 777)
 	});
 
 
 
 	var files = fs.readdirSync(backup_path)
+
 	files.forEach(function(name){
 		try{
-			var filetime = moment(name).millisecond() 
-			var now = Date.now()
+			var filetime = moment(name).unix()*1000
+			var now = Date.now()-0
 			if(now - filetime >1000*3600*24*7){ //删除7天前的记录
+				console.log(name)
 				fs.unlinkSync(backup_path+name);
 			}
 		}
