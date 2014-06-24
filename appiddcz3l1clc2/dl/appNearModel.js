3c0ea8b3e1902a99154f6fae9a2403c1d2df55da
spout <snoopyxdy@gmail.com>
2014-06-24 11:09:30 +0800

    123

--- appiddcz3l1clc2/dl/appNearModel.js
+++ appiddcz3l1clc2/dl/appNearModel.js
@@ -6,7 +6,7 @@ var obj = { //定义结构
       coNearName:{type:String,required:true},                		        //配套名称
       coNearAddr:{type:String,required:true},      						//配套地址
       coNearDesc:{type:String,required:true},            		     	//配套描述
-      coNearTel:{type:Number,required:true},            			    //配套电话
+      coNearTel:{type:String,required:true},            			    //配套电话
       isShow:{type:Number,default:1},                        //1表示启用，0表示不启用
 	    writeTime: { type: Date, default: function(){return Date.now()} },    //写入时间
 }
