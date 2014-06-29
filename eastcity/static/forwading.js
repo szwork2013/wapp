f4f6f702947ac13afc5ed05bb624cbc5a688e434
snoopyxdy <snoopyxdy@gmail.com>
2014-06-29 20:35:14 +0800

    11

--- eastcity/static/forwading.js
+++ eastcity/static/forwading.js
@@ -1,3 +1,98 @@
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+WeixinJSBridge.call('hideToolbar');
+});
+function attention_wx()
+{
+     alert(window.articleid)
+     if(window.articleid){
+        $.post('/api/score/forwarding',{'articleid':window.articleid,'wxuserid':window.userid},function(d){
+            alert(JSON.stringify(d))
+        },'json')
+    }
+}
+function is_zero()
+{
+    if(share_score == 0)
+    {
+        share_score = 2000;
+    }
+}
+var dataForWeixin = {
+    appId: "",
+    MsgImg: "http://imgdn5.soufunimg.com/2014/04/07/gz/gebz/ab601d81e4724a4a8bd5ac01ee66ee62/mlylogo.jpg",
+    TLImg: "http://imgdn5.soufunimg.com/2014/04/07/gz/gebz/ab601d81e4724a4a8bd5ac01ee66ee62/mlylogo.jpg",
+    url:  "http://imgdn5.soufunimg.com/2014/04/07/gz/gebz/ab601d81e4724a4a8bd5ac01ee66ee62/",
+    title: "我挑战了“搜房豪宅之梦”快来一起挑战吧！",
+    desc: "我挑战了“搜房豪宅之梦”，获得了" + share_score + "分的好成绩！你也来试试吧!",
+    fakeid: "",
+    callback: function() {
+        setTimeout(function(){
+          attention_wx();
+         },2000);       }
+}; (function() {
+    var onBridgeReady = function() {
+        WeixinJSBridge.on('menu:share:appmessage',
+        function(argv) {
+            is_zero();
+            WeixinJSBridge.invoke('sendAppMessage', {
+                "appid": dataForWeixin.appId,
+                "img_url": dataForWeixin.MsgImg,
+                "img_width": "120",
+                "img_height": "120",
+                "link": dataForWeixin.url,
+                "desc": "我挑战了“搜房豪宅之梦”获得了" + share_score + "分的好成绩！你也来试试吧！",
+                "title": "我挑战了“搜房豪宅之梦”获得了" + share_score + "分的好成绩！你也来试试吧!"
+            },
+            function(res) { (dataForWeixin.callback)();
+            });
+        });
+        WeixinJSBridge.on('menu:share:timeline',
+        function(argv) { (dataForWeixin.callback)();
+        is_zero();
+            WeixinJSBridge.invoke('shareTimeline', {
+                "img_url": dataForWeixin.TLImg,
+                "img_width": "120",
+                "img_height": "120",
+                "link": dataForWeixin.url,
+                "desc": "我挑战了“搜房豪宅之梦”获得了" + share_score + "分的好成绩！你也来试试吧！",
+                "title": "我挑战了“搜房豪宅之梦”获得了" + share_score + "分的好成绩！你也来试试吧！"
+            },
+            function(res) {});
+        });
+        WeixinJSBridge.on('menu:share:weibo',
+        function(argv) {
+            WeixinJSBridge.invoke('shareWeibo', {
+                "content": dataForWeixin.title,
+                "url": dataForWeixin.url
+            },
+            function(res) { (dataForWeixin.callback)();
+            });
+        });
+        WeixinJSBridge.on('menu:share:facebook',
+        function(argv) { (dataForWeixin.callback)();
+            WeixinJSBridge.invoke('shareFB', {
+                "img_url": dataForWeixin.TLImg,
+                "img_width": "120",
+                "img_height": "120",
+                "link": dataForWeixin.url,
+                "desc": dataForWeixin.desc,
+                "title": dataForWeixin.title
+            },
+            function(res) {});
+        });
+    };
+    if (document.addEventListener) {
+        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+    } else if (document.attachEvent) {
+        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+    }
+})();
+
+
+
+
+
 
 
 var WeixinApi = (function () { 
@@ -16,7 +111,7 @@ var WeixinApi = (function () {
         imagePreview    :imagePreview
     };    
  
-});
+})();
 // 所有功能必须包含在 WeixinApi.ready 中进行
 WeixinApi.ready(function(Api){
     alert(111)
@@ -46,12 +141,7 @@ WeixinApi.ready(function(Api){
         },
         // 分享成功
         confirm:function (resp) {
-                 alert(window.articleid)
-                 if(window.articleid){
-                    $.post('/api/score/forwarding',{'articleid':window.articleid,'wxuserid':window.userid},function(d){
-                        alert(JSON.stringify(d))
-                    },'json')
-                }
+                
         },
         // 整个分享过程结束
         all:function (resp) {
