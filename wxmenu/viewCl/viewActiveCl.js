93c8b4e1cf5534c27d8c2f075543fc97ab7b4488
snoopyxdy <snoopyxdy@gmail.com>
2015-01-23 22:16:31 +0800

    获取用户oauth信息

--- wxmenu/viewCl/viewActiveCl.js
+++ wxmenu/viewCl/viewActiveCl.js
@@ -166,6 +166,9 @@ obj.activePage = function(req,res){ //活动页面展示
 		var fromUserObj = {
 			_id:req.fromUserObj._id,
 			appUserName:req.fromUserObj.appUserName,
+			wxName:req.fromUserObj.wxName,
+			wxAvatar:req.fromUserObj.wxAvatar,
+			wxAddress:req.fromUserObj.wxAddress,
 			appUserMobile:req.fromUserObj.appUserMobile
 		}
 		if(req.fromUserObj.appUserName == '未认证会员'){
