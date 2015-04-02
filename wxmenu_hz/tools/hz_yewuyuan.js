e8ac07a038918d280bb9c7aa041b762b75802d40
spout <snoopyxdy@gmail.com>
2015-04-02 08:43:56 +0800

    lo

---
+++ wxmenu_hz/tools/hz_yewuyuan.js
@@ -0,0 +1,37 @@
+var obj={}
+obj.yewuyuan_list  = []
+
+
+obj.get_yewuyuan = function(wxname){
+
+
+	return false
+
+}
+
+
+obj.get_number = function(replyStr){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = obj
