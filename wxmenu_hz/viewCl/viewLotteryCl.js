526c5916d111391032e351a2a7227aa8597e028f
snoopyxdy <snoopyxdy@gmail.com>
2015-05-01 10:37:59 +0800

    抽奖bug

--- wxmenu_hz/viewCl/viewLotteryCl.js
+++ wxmenu_hz/viewCl/viewLotteryCl.js
@@ -13,7 +13,7 @@ obj.lotteryPage = function(req,res){ //活动页面展示
     res.set('ETag', Date.now().toString())
     var requestedUrl = req.protocol + '://' + req.get('Host') + req.url;
 
-	var wxuobj = req.wxuobj;
+	//var wxuobj = req.wxuobj;
 
 	var lotteryEname = req.query.ename;
 	//业务员用户id
@@ -41,72 +41,78 @@ obj.lotteryPage = function(req,res){ //活动页面展示
 	}
 
 
-
 	//获取业务员的信息
-	userBl.getUserByUserId(ywyuserid, function(err, ywyObj){
-		if(err){
-			return res.send(err)
-		}
-		if(!ywyObj){
-			return res.send('未找到业务员')
-		}
-
-		if(ywyObj.uobj.appUserType != 2){
-			return res.send('业务员类型有误')
-		}
-		
-		var ywy_userid = ywyObj.uobj._id
-		var ywy_mobile = ywyObj.uobj.appUserMobile
-
-		//先去查找抽奖活动记录
-		lotteryBl.getLotteryByEname(lotteryEname,function(err,lotteryObj){
+	userBl.getUserByUserId(userid, function(err, wxuobj){
 			if(err){
 				return res.send(err)
 			}
-			if(!lotteryObj){
-				return res.send(404)
-			}
+			var wxuobj = wxuobj.uobj
 
-			appBl.getByEname(appEname,function(err,appObj){
+			//获取业务员的信息
+			userBl.getUserByUserId(ywyuserid, function(err, ywyObj){
 				if(err){
 					return res.send(err)
 				}
-				if(!appObj){
-					return res.send(404)
+				if(!ywyObj){
+					return res.send('未找到业务员')
 				}
 
-				var timeError = 0
-				var now = moment()
-				var startTime = moment(lotteryObj.startTime)
-				var endTime = moment(lotteryObj.endTime)
-				if(now<startTime || now>endTime){
-					timeError = 1
+				if(ywyObj.uobj.appUserType != 2){
+					return res.send('业务员类型有误')
 				}
+				
+				var ywy_userid = ywyObj.uobj._id
+				var ywy_mobile = ywyObj.uobj.appUserMobile
 
-				res.render('lottery/'+lotteryEname+'.ejs', {
-					lotteryObj:lotteryObj,
-					lotteryEname:lotteryEname,
-					appEname:appEname,
-					appId:appObj._id,
-					userid:userid,
-					ywyuserid:ywyuserid,
-					wxuobj:wxuobj,
-					timeError:timeError,
-					ywy_userid:ywy_userid,
-					ywy_mobile:ywy_mobile,
-					jsurl:encodeURIComponent(requestedUrl)
-				}); //end render
+				//先去查找抽奖活动记录
+				lotteryBl.getLotteryByEname(lotteryEname,function(err,lotteryObj){
+					if(err){
+						return res.send(err)
+					}
+					if(!lotteryObj){
+						return res.send(404)
+					}
+
+					appBl.getByEname(appEname,function(err,appObj){
+						if(err){
+							return res.send(err)
+						}
+						if(!appObj){
+							return res.send(404)
+						}
+
+						var timeError = 0
+						var now = moment()
+						var startTime = moment(lotteryObj.startTime)
+						var endTime = moment(lotteryObj.endTime)
+						if(now<startTime || now>endTime){
+							timeError = 1
+						}
+
+						res.render('lottery/'+lotteryEname+'.ejs', {
+							lotteryObj:lotteryObj,
+							lotteryEname:lotteryEname,
+							appEname:appEname,
+							appId:appObj._id,
+							userid:userid,
+							ywyuserid:ywyuserid,
+							wxuobj:wxuobj,
+							timeError:timeError,
+							ywy_userid:ywy_userid,
+							ywy_mobile:ywy_mobile,
+							jsurl:encodeURIComponent(requestedUrl)
+						}); //end render
 
 
-			})//end appBl.getByEname(appEname,function(err,appObj){
+					})//end appBl.getByEname(appEname,function(err,appObj){
 
 
-		})//end lotteryBl.getLotteryByEname(lotteryEname,function(err,lotteryObj){
+				})//end lotteryBl.getLotteryByEname(lotteryEname,function(err,lotteryObj){
 
 
-	})//end userBl.getUserByUserId(ywy_userid, function(err, ywyObj){
+			})//end userBl.getUserByUserId(ywy_userid, function(err, ywyObj){
 
-	
+	})//end 
 
 	
 
