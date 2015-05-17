82176e98e87b7d194974c3871d280483c2126697
snoopyxdy <snoopyxdy@gmail.com>
2015-05-17 12:47:45 +0800

    go

--- wxmenu/static/avengers/js/index.js
+++ wxmenu/static/avengers/js/index.js
@@ -1,63 +1,27 @@
-window.onerror = function (errorMsg, url, lineNumber) {
-
-    //alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber);
-
-}
-
+var rot = 0;
 $(function(){
-	//注册微信接口
-  // url = location.href;
-  // url = url.replace("&","55555");
-  // $.ajax({
-  //   type : "get",
-  //   url : "http://middleware.siyanhui.com:3000/wechat/token?url="+url,
-  //   dataType : "jsonp",
-  //   jsonp: "callback",
-  //   jsonpCallback:"success_jsonpCallback",
-  //   success : function(data){ 
-  //       wx.config({
-  //         appId: 'wx05707a5b58cabc7f',
-  //         timestamp: data.timestamp,
-  //         nonceStr: data.nonceStr,
-  //         signature: data.signature,
-  //         jsApiList: [
-  //           "onMenuShareTimeline",
-  //           "onMenuShareAppMessage",
-  //           "onMenuShareQQ",
-  //           "onMenuShareWeibo"
-  //         ]
-  //       });
-  //       wx.error(function(res){
-
-		//     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-		// 	console.log(res);
-		// 	// alert("wx:error");
-		// 	// alert(res);
-		// });
-  //   },
-  //   error:function(data){
-  //       alert("连接失败！");
-  //   }
-  // });
-  
-//localStorage.removeItem("myId");
+	localStorage.removeItem("myId");
 	//判断本地是否有存储id，有则直接展示，没有则开始新的页面
 	if(localStorage.myId){
 		$(".loadingModal").addClass('show');
 
-		$.ajax({
-			'url':'http://apps.me-link.com/open/avengers/get',
-			'data':{
-				'id':localStorage.myId
-			},
-			'dataType':'jsonp',
-			'error':function(){
-				
-			},
-			'jsonp':'callback',
-			'success':function(result){
+		var infoAjax = $.ajax({
+			url:'http://apps.me-link.com/open/avengers/get',
+			data:{'id':localStorage.myId},			
+			dataType:'jsonp',
+			error:function(){},
+			jsonp:'callback',
+			success:function(result){
 				setInfo(result);
-			}
+			},
+			timeout : 60000, 
+			complete : function(XMLHttpRequest,status){ 
+		　　　　if(status=='timeout'){
+		 　　　　　 infoAjax.abort();
+		 			$(".modal").removeClass('show');
+		　　　　　  $(".netFailedModal").addClass('show');
+		　　　　}
+		　　}
 		})
 
 	} else {
@@ -65,7 +29,8 @@ $(function(){
 	}
 	
 	//绑定事件
-	$(".photo footer a").on("click",changePhoto);
+	$("#rotatePhoto").on("click",rotatePhoto);
+	$("#changePhoto").on("click",changePhoto);
 	//$(".photo footer button").on("click",usePhoto);
 	$(".modal").click(function(){
 		if($(this).hasClass('loadingModal')){
@@ -86,6 +51,7 @@ $(function(){
 window.imgDataBase64 = false
 function bindInput(){
 	$('#selectPhoto').change(function(){
+		// console.log(event);
 		var img = event.target.files[0]; 
 		
 		// 判断是否图片  
@@ -107,50 +73,7 @@ function bindInput(){
         reader.readAsDataURL(img);  
         reader.onload = function(e){ // reader onload start  
         	window.imgDataBase64 = e.target.result
-
-
-
-
-        	var image = new Image(); 
-        	var MAX_HEIGHT = 400; 
-        	image.onload = function(){ 
-
-        		var canvas = document.getElementById("myCanvas");  
-		        // 如果高度超标  
-		        if(image.height > MAX_HEIGHT) {  
-		            // 宽度等比例缩放 *=  
-		            image.width *= MAX_HEIGHT / image.height;  
-		            image.height = MAX_HEIGHT;  
-		        }  
-		        // 获取 canvas的 2d 环境对象,  
-		        // 可以理解Context是管理员，canvas是房子  
-		        var ctx = canvas.getContext("2d");  
-		        // canvas清屏  
-		        ctx.clearRect(0, 0, canvas.width, canvas.height);  
-		        // 重置canvas宽高  
-		        canvas.width = image.width;  
-		        canvas.height = image.height;  
-		        // 将图像绘制到canvas上  
-		        ctx.drawImage(image, 0, 0, image.width, image.height);  
-
-		        window.imgDataBase64 = canvas.toDataURL("image/png")
-
-		        $(".photo .picWrapper div").css({
-					"background":"url(" + window.imgDataBase64 + ") no-repeat center center",
-					"background-size":"contain"
-				  });
-	            $(".index").removeClass('active');              
-	            $(".photo").addClass('active');
-
-        	}
-        	image.src = window.imgDataBase64;  
-
-
-        	
-
-
-
-            
+			initCanvas();
         } // reader onload end  
 
 	})
@@ -162,23 +85,65 @@ function bindInput(){
 		$(".uploadModal").addClass('show');
 		ajaxIng = true
 		//上传图片
-		$.post('/api/base64/upload', {
-			imgData:window.imgDataBase64,
-			fileType:'png'
-			}, function(d){
-				ajaxIng = false
+		var uploadAjax = $.ajax({
+			url:'/api/base64/upload',
+			type:"POST",
+			data:{
+				imgData:window.imgDataBase64,
+				fileType:'png'
+			},
+			timeout:60000,
+			dataType:"json",
+			success:function(d){
+				ajaxIng = false;
 				if(d.error == 1){
 					$(".failedModal").addClass('show');
 					return
 				}
-				var imgUrl = 'http://'+location.host+d.data
-
-
-				//alert(imgUrl)
-				usePhoto(imgUrl)
-		}, 'json')
-
+				var imgUrl = 'http://'+location.host+d.data;
+				usePhoto(imgUrl);
+			},
+			complete : function(XMLHttpRequest,status){ 
+		　　　　if(status=='timeout'){
+		 　　　　　 uploadAjax.abort();
+		 			$(".modal").removeClass('show');
+		　　　　　  $(".netFailedModal").addClass('show');
+		　　　　}
+		　　}
+		});
 	})
+}
+
+function initCanvas(){
+	var image = new Image(); 
+	var MAX_HEIGHT = 400; 
+	image.onload = function(){ 
+		var canvas = document.getElementById("myCanvas");  
+        // 如果高度超标  
+        if(image.height > MAX_HEIGHT) {  
+            // 宽度等比例缩放 *=  
+            image.width *= MAX_HEIGHT / image.height;  
+            image.height = MAX_HEIGHT;  
+        }  
+        // 获取 canvas的 2d 环境对象,  
+        // 可以理解Context是管理员，canvas是房子  
+        var ctx = canvas.getContext("2d");  
+        // canvas清屏  
+        ctx.clearRect(0, 0, canvas.width, canvas.height);  
+        // 重置canvas宽高  
+        canvas.width = image.width;  
+        canvas.height = image.height;  
+        // 将图像绘制到canvas上  
+        ctx.drawImage(image, 0, 0, image.width, image.height);  
+        window.imgDataBase64 = canvas.toDataURL("image/png")
+        $(".photo .picWrapper div").css({
+			"background":"url(" + window.imgDataBase64 + ") no-repeat center center",
+			"background-size":"contain"
+		  });
+        $(".index").removeClass('active');              
+        $(".photo").addClass('active');
+	}
+	image.src = window.imgDataBase64;  
 }
 
 function MathRand(length){ 
@@ -197,6 +162,23 @@ function changePhoto(){
   	$('#selectPhoto').val('')
 }
 
+function rotatePhoto(){
+	var file = document.getElementById("selectPhoto").files[0];
+	rot = rot + 90;
+	if(rot == 360){
+		rot = 0;
+	}
+	canvasResize(file, {
+        crop: false,
+        quality: 80,
+        rotate: rot,
+        callback: function(data, width, height) {
+        	window.imgDataBase64 = data;
+            initCanvas();
+        }
+    });
+}
+
 function downloadApp(){
 	startDownloadApp();
 }
@@ -205,19 +187,19 @@ function usePhoto(imgWebUrl){
 	//$(".loadingModal").addClass('show');
 	var imgUrl = imgWebUrl
   	
-	$.ajax({
-		'url':'http://apps.me-link.com/open/avengers/play',
-		'data':{
-			'file_url':imgUrl
+	var usePhotoAjax = $.ajax({
+		url:'http://apps.me-link.com/open/avengers/play',
+		data:{
+			file_url:imgUrl
 		},
-		'dataType':'jsonp',
-		'error':function(){
+		dataType:'jsonp',
+		error:function(){
 			$(".loadingModal").removeClass('show');
 			$(".uploadModal").removeClass('show');
     		$(".failedModal").addClass('show');
 		},
-		'jsonp':'callback',
-		'success':function(result){
+		jsonp:'callback',
+		success:function(result){
 
 			if(result.status === "ok"){
 	    		localStorage.myId=result.result.Id;
@@ -229,7 +211,15 @@ function usePhoto(imgWebUrl){
 	    	}
     		$(".loadingModal").removeClass('show');
     		$(".uploadModal").removeClass('show');
-		}
+		},
+		timeout : 60000, 
+		complete : function(XMLHttpRequest,status){ 
+	　　　　if(status=='timeout'){
+	 　　　　　 usePhotoAjax.abort();
+	 			$(".modal").removeClass('show');
+	　　　　　  $(".netFailedModal").addClass('show');
+	　　　　}
+	　　}
 	})
 }
 
@@ -253,12 +243,8 @@ function setInfo(obj){
 }
 
 function clickShare(data){
-	
-
 	wx.config(window.jsconfig);
-	//alert(window.jsticket_error)
 	wx.ready(function(){
-			//alert(JSON.stringify(data))
 			var shareOption = {
 				title: '穿越！快来测试你是复仇者联盟2里面的谁谁谁?', // 分享标题
 			    link: 'http://melink.siyanhui.com/static/avengers/share.html?r='+Date.now()+'&id=' + data.Id, // 分享链接
